f7e7944a0e293fb93cadac0849a36d4afffd5380
ShivajiGunde <shivajigunde4@gmail.com>
2026-03-13 09:03:38 +0530

    adding pigdice

---
+++ PigDice/src/test/PigDice.java
@@ -0,0 +1,63 @@
+package test;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class PigDice {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+
+		int totalScore = 0;
+		final int targetScore = 20;
+
+		System.out.println("Let's Play PIG!");
+		System.out.println("Reach 20 points to win.\n");
+
+		for (int turn = 1; totalScore < targetScore; turn++) {
+
+			int turnScore = 0;
+			System.out.println("Turn " + turn);
+
+			for (boolean turnOver = false; !turnOver;) {
+
+				System.out.print("Roll or hold? (r/h): ");
+				String choice = scanner.nextLine().toLowerCase();
+
+				if (choice.equals("r")) {
+
+					int die = random.nextInt(6) + 1;
+					System.out.println("Die: " + die);
+
+					if (die == 1) {
+						turnScore = 0;
+						turnOver = true;
+						System.out.println("Turn over. No score.\n");
+					} else {
+						turnScore += die;
+					}
+
+				} else if (choice.equals("h")) {
+
+					totalScore += turnScore;
+					turnOver = true;
+
+					System.out.println("Score for turn: " + turnScore);
+					System.out.println("Total score: " + totalScore + "\n");
+					
+					if (totalScore >= targetScore) {
+				        System.out.println("You reached 20 points or more. You win");
+				        break;
+				    }
+
+				} else {
+					System.out.println("Invalid input. Enter 'r' or 'h'.");
+				}
+			}
+		}
+
+		scanner.close();
+	}
+
+}
